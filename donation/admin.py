c3fd913dc0916c898b85c5ba6eda31d84c759e2c
adnan683515 <golamfaruk680@gmail.com>
2025-04-02 15:58:45 +0600

    update

--- donation/admin.py
+++ donation/admin.py
@@ -6,36 +6,4 @@ from .models import DonateBlood,BloodRequest
 
 
 admin.site.register(DonateBlood)
-
-
-
-class ModelAdminBloodRequest(admin.ModelAdmin):
-    list_display  = ['user','numberOfBag','donateDate','place','mobile','status']
-    
-
-    def numberOfBag(self,obj):
-        return obj.numberOfBag
-    
-    def donateDate(self,obj):
-        return obj.donateDate
-    
-    def place(self,obj):
-        return obj.place
-    
-    def email(self,obj):
-        return obj.user.email
-    
-    
-    
-
-    def save_model(self, request, obj, form, change):
-        obj.save()
-        if obj.status == 'Accepted':
-            mail_sub = 'Congress!!'
-            email_body = render_to_string('sendReqEmail.html',{'user':obj.user,'DonateBlood':obj.DonateBlood})
-            email = EmailMultiAlternatives(mail_sub,'',to=[obj.user.email])
-            email.attach_alternative(email_body,'text/html')
-            email.send()
-            
-    
-admin.site.register(BloodRequest,ModelAdminBloodRequest)
+admin.site.register(BloodRequest)
